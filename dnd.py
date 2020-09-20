732ebed2668d1d1902f1e837f01782ac308e1bcc
brickviking <brickviking@gmail.com>
2020-09-21 00:11:50 +1200

    Update dnd.py
    Expanded to single line output, upped to maximum of 100 dice, and included d20 and d100.

--- dnd.py
+++ dnd.py
@@ -1,16 +1,24 @@
+#!/usr/bin/env python3
+# Origin: https://github.com/zwilliamsdev/dnd_dice/blob/master/dnd.py
+# Modifications: https://github.com/brickviking/dnd_dice/dnd.py
+
 import sys
 import random
 
 # Check provided user input
 def verifyNumArgs(check):
+    # Need a way to convert a string like 3d6 instead of the separate two integers
     if len(check) == 3:
        # Only two arguments provided, we can proceed
        convertStringToInt(check)
     else:
-        print("Invalid arguments. Use: dnd.py 'numDice' 'numSides' (dnd.py 3 12)")
-        # Return immeidatly stopping code
-        return
+        usageString
 
+def usageString():
+        print("Invalid arguments. Use: dnd.py 'numDice' 'numSides' (dnd.py 3 12)")
+        # Return immediately stopping code
+        return
+    
 # Ensure correct values for arguments
 def convertStringToInt(check):
     # Verify conversion
@@ -33,23 +41,24 @@ def convertStringToInt(check):
 
 # Check that user values make sense
 def verifyRollAmounts(numDice, numSides):
-    maxDice = 5
+    maxDice = 100
     # Cannot roll 0 dice or dice with 0 sides
     if(numDice == 0 or numSides == 0):
         # Print message to user
         print("Number of dice or sides cannot be zero.")
         # Return stopping code immediately
         return
-    # Cannot roll more than 5 dice at once
+    # Cannot roll more than maxDice at once
     if(numDice > maxDice):
+        # No real reason why there's an arbitrary limit, let's leave it to 100 dice
         # Print message to user
-        print("You can roll a maximum of " + maxDice + " dice.")
+        print("You can roll a maximum of " + str(maxDice) + " dice.")
         # Return stopping code immediately
         return
-    # Sides must be divisible by 2, at least 2, and at most 12
-    if(numSides % 2 != 0 or numSides < 2 or numSides > 12):
+    # Sides must be the following: 2, 4, 6, 8, 10, 12, 20,100
+    if(numSides % 2 != 0 or numSides < 2 or (numSides > 12 and numSides < 20) or (numSides > 20 and numSides != 100)):
         # Print message to user
-        print("Allowed dice sides: 2, 4, 6, 8, 10, and 12.")
+        print("Allowed dice sides: 2, 4, 6, 8, 10, 12, 20 and 100.")
         # Return stopping code immediately
         return
     # All tests have passed we can now safely roll the dice!
@@ -59,22 +68,31 @@ def rollDice(numDice, numSides):
     rolls = 0
     minNumSides = 2
     value = 0
+    # need an empty string in here
+    out = ""
+    out = str(numDice) + "d" + str(numSides) + ": "
     while rolls < numDice:
         roll = random.randint(minNumSides, numSides)
         value += roll
-        print("You roll " + str(roll))
+#        print(str(roll))
         rolls += 1
-    print("You rolled a total of " + str(value))
+        out = out + str(rolls) + ":" + str(roll) + " "
+    out = out + "Total of " + str(value) + "/" + str(numSides*numDice)
+    print(out)
+
 
 class DND:
     ###
     # TODO: Allow multiple pairs of arguments to roll multiple times
     #       Example: dnd.py 1 6 2 12
+    # TODO: Allow string inputs like dnd.py 3d6 1d20
     # TODO: Create a help command to tell user how to use program
-    # TODO: Create global rules for number of dice, number of sides
+    # TODO: Create global rules for number of dice, number of sides (nearly done)
     ### 
 
     # Grab arguments
     userInput = sys.argv
     # Kick off program
     verifyNumArgs(userInput)
+
+    
